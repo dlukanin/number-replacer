f4cb50de459a3d7f4fe0e1425ec8fdab0da0a6b0
krigar1184 <krigar1184@gmail.com>
2017-05-28 23:03:46 +0300

    Alternate version

--- test/main.js
+++ test/main.js
@@ -1,6 +1,7 @@
 var chai = require('chai');
 var Replacer = require('../lib/replacer.js').Replacer;
 var expect = chai.expect;
+var replace = require('../lib/replace.js').replace;
 
 describe('replacer', function() {
     it('should replace numbers in simple number value', function() {
@@ -11,5 +12,21 @@ describe('replacer', function() {
     it('should replace numbers in float', function() {
         var replacer = new Replacer();
         expect(replacer.replace(12.345)).to.eq('****45');
+    });
+});
+
+describe('replacer 2', function() {
+    it('should replace numbers in simple number value', function() {
+        expect(replace(123456)).to.eq('***456');
+        expect(replace('123456')).to.eq('***456');
+    });
+
+    it('should replace numbers in float', function() {
+        expect(replace(12.345)).to.eq('****45');
+        expect(replace('12.345')).to.eq('****45');
+    });
+
+    it('should fail', function() {
+
     });
 });
